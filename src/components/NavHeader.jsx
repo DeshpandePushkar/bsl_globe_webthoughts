9c124871fb5bc18e0080d3d63d7ee6e1fc7ee7dc
DeshpandePushkar <deshpandepushkar@bitbucket.org>
2025-09-22 19:14:27 +0530

    Nav header redirection transformmed json files

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NavHeader = () => {
+  const navigate = useNavigate();
   return (
     <nav className="nav-header">
       <div className="nav-container">
         <div className="nav-content">
-          <img src="/assets/logo.svg" alt="Globe Logo" className="nav-logo" />
+          <img
+            src="/assets/logo.svg"
+            alt="Globe Logo"
+            className="nav-logo"
+            onClick={() => navigate("/")}
+          />
         </div>
       </div>
     </nav>
